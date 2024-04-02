2d475040efcde245b1e63ebf35e8d03c7e3bb589
Hidekazu Ikeno <hide.ikeno@gmail.com>
2024-04-02 22:02:28 +0900

    add vial config for mothwing

---
+++ keyboards/mothwing/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x7B, 0xBB, 0x69, 0x34, 0x11, 0x8A, 0xF3, 0x90}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 7}
+#define VIAL_UNLOCK_COMBO_COLS {0, 2}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
