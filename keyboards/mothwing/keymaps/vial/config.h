f26ff7fc50143b446287b26117040cd378c0fe1f
Hidekazu Ikeno <hide.ikeno@gmail.com>
2024-04-16 22:05:57 +0900

    add achordion lib

--- keyboards/mothwing/keymaps/vial/config.h
+++ keyboards/mothwing/keymaps/vial/config.h
@@ -9,22 +9,22 @@
 #define VIAL_COMBO_ENTRIES 10
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
 
-#define ENABLE_COMPILE_KEYCODE
-
 // Tap-Hold configuration for home-row-mods
-
 #define TAPPING_TERM 175
-// #define PERMISSIVE_HOLD
-// #define QUICK_TAP_TERM_PER_KEY
+#define TAPPING_TERM_PER_KEY
+#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM_PER_KEY
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
 
 // Use Left Ctrl + Right Ctrl to activate the Command feature.
 #define IS_COMMAND() (get_mods() == MOD_MASK_CTRL)
-
 // Holding Shift while Caps Word is active inverts the shift state.
 #define CAPS_WORD_INVERT_ON_SHIFT
 // When idle, turn off Caps Word after 5 seconds.
 #define CAPS_WORD_IDLE_TIMEOUT 5000
 
 // for Achordion library
-// #define ACHORDION_STREAK
+#define ACHORDION_STREAK
 
--- keyboards/mothwing/keymaps/vial/keymap.c
+++ keyboards/mothwing/keymaps/vial/keymap.c
@@ -15,6 +15,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+#include "features/achordion.h"
+
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
@@ -41,6 +43,16 @@ enum custom_keycodes {
 #define KC_G_EN LGUI_T(KC_LNG2)      // cmd or win
 #define KC_C_BS LCTL_T(KC_BSPC)      // ctrl
 #define KC_A_DEL ALT_T(KC_DEL)       // alt
+
+// Home row mods (QWERTY)
+#define HOME_A LCTL_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LGUI_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RGUI_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SC RCTL_T(KC_SCLN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
@@ -79,6 +91,76 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //                  `--------+--------+--------+--------'   `--------+--------+--------+--------'
     ),
 };
+
+// uint16_t get_tapping_term(uint16_t keycode, keyrecord_t* record) {
+//     switch (keycode) {
+//         // Increase the tapping term a little for slower ring and pinky fingers.
+//         case HOME_A:
+//         case HOME_S:
+//         case HOME_L:
+//         case HOME_SC:
+//             return TAPPING_TERM + 15;
+//
+//         default:
+//             return TAPPING_TERM;
+//     }
+// }
+
+uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t* record) {
+    // If you quickly hold a tap-hold key after tapping it, the tap action is
+    // repeated. Key repeating is useful e.g. for Vim navigation keys, but can
+    // lead to missed triggers in fast typing. Here, returning 0 means we
+    // instead want to "force hold" and disable key repeating.
+    switch (keycode) {
+        // Repeating is useful for Vim navigation keys.
+        case HOME_J:
+        case HOME_K:
+        case HOME_L:
+            return QUICK_TAP_TERM;  // Enable key repeating.
+        default:
+            return 0;  // Otherwise, force hold and disable key repeating.
+    }
+}
+
+bool achordion_chord(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
+        uint16_t other_keycode, keyrecord_t* other_record) {
+    // Exceptionally consider the following chords as holds, even though they
+    // are on the same hand in Magic Sturdy.
+    switch (tap_hold_keycode) {
+        case HOME_A:  // A + F and A + G.
+            if (other_keycode == HOME_F || other_keycode == KC_G || other_keycode == KC_B) {
+                return true;
+            }
+            break;
+    }
+
+    // // Also allow same-hand holds when the other key is in the rows below the
+    // // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboard is split.
+    // if (other_record->event.key.row % (MATRIX_ROWS / 2) >= 4) {
+    //     return true;
+    // }
+
+    // Otherwise, follow the opposite hands rule.
+    return achordion_opposite_hands(tap_hold_record, other_record);
+}
+
+uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
+    return 800;  // Use a timeout of 800 ms.
+}
+
+uint16_t achordion_streak_timeout(uint16_t tap_hold_keycode) {
+    if (IS_QK_LAYER_TAP(tap_hold_keycode)) {
+        return 0;  // Disable streak detection on layer-tap keys.
+    }
+
+    // Otherwise, tap_hold_keycode is a mod-tap key.
+    uint8_t mod = mod_config(QK_MOD_TAP_GET_MODS(tap_hold_keycode));
+    if ((mod & MOD_LSFT) != 0) {
+        return 0;  // Disable for Shift mod-tap keys.
+    } else {
+        return 100;
+    }
+}
 
 // Callback for the Caps Word features
 bool caps_word_press_user(uint16_t keycode) {
@@ -191,7 +273,8 @@ static void magic_send_string_P(const char* str, uint16_t repeat_keycode) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-    // if (!process_achordion(keycode, record)) { return false; }
+    if (!process_achordion(keycode, record)) { return false; }
+
     // Your macros ...
     if (record->event.pressed) {
         switch (keycode) {
@@ -221,6 +304,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     return true;
 }
 
-// void matrix_scan_user(void) {
-//     achordion_task();
-// }
+void matrix_scan_user(void) {
+    achordion_task();
+}
